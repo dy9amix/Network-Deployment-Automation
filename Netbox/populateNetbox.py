a3e7724c94d4c82778fcbfe0dac8dd4beb67771b
orodeji oluwasegun <32060683+dy9amix@users.noreply.github.com>
2021-03-18 16:05:30 +0100

    Update populateNetbox.py

--- Netbox/populateNetbox.py
+++ Netbox/populateNetbox.py
@@ -1,4 +1,5 @@
 import os
+import pprint
 from genie import testbed
 from pyats import topology
 from pyats.topology import loader
@@ -6,15 +7,15 @@ from nornir import InitNornir
 from nornir_utils.plugins.functions import print_result 
 from nornir.core.filter import F
 
-testbedfile = (os.getcwd()+'/testbed.yaml')
-testbed = topology.loader.load(testbedfile)
+testbed = testbed.load('testbed.yaml')
 nr = InitNornir("config.yaml")
 
 
 def run_parsers(task):
     device = testbed.devices[f'{task.host}']
-    connect = device.connect()
-    output = connect.parse('show interfaces')
+    device.connect()
+    output = device.parse('show interfaces')
+    pprint(output)
 
 def main():
     nr.run(task=run_parsers)
