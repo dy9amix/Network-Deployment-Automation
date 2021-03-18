339b60c27d4ffd60557ad8fce35bd734943f79cf
orodeji oluwasegun <32060683+dy9amix@users.noreply.github.com>
2021-03-18 16:08:56 +0100

    Update populateNetbox.py

--- Netbox/populateNetbox.py
+++ Netbox/populateNetbox.py
@@ -2,12 +2,13 @@ import os
 import pprint
 from genie import testbed
 from pyats import topology
+from genie.conf import Genie
 from pyats.topology import loader
 from nornir import InitNornir
 from nornir_utils.plugins.functions import print_result 
 from nornir.core.filter import F
 
-testbed = testbed.load('testbed.yaml')
+testbed = Genie.init('testbed.yaml')
 nr = InitNornir("config.yaml")
 
 
