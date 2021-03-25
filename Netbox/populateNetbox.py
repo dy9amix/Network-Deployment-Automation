3d88efb78d1d1f7f9b712f28c3c3535f4aaec037
dy9amix <segunorodeji@gmail.com>
2021-03-25 17:50:07 +0100

    SW-11 and 12 added to topology with correct role name

--- Netbox/populateNetbox.py
+++ Netbox/populateNetbox.py
@@ -40,7 +40,7 @@ def run_parsers(task):
 
 #Text edit
 def main():
-    client_core = nr.filter(device_role='core')
+    client_core = nr.filter(role="Core")
     parser_task = client_core.run(task=run_parsers)
     print_result(parser_task)
 
